892073b92b27914f0cd33a718d2e3d540cbeac1c
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-01-30 20:54:07 +0100

    action_runners: Add the ActionRunnerBase class

---
+++ tests/test_action_runners_base.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of walt
+# https://github.com/scorphus/walt
+
+# Licensed under the BSD-3-Clause license:
+# https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2021, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+from unittest.mock import AsyncMock
+from walt.action_runners import ActionRunnerBase
+
+import asyncio
+import os
+import pytest
+import signal
+
+
+class ActionRunnerBaseTester(ActionRunnerBase):
+    async def _run_action(self):
+        for task in self.tasks:
+            try:
+                self._create_task(*task)
+            except TypeError:
+                self._create_task(task)
+        await super()._run_action()
+
+    def register_tasks(self, tasks):
+        self.tasks = tasks
+
+
+@pytest.fixture
+def action_runner():
+    return ActionRunnerBaseTester()
+
+
+def test_action_runner_runs_a_single_task(action_runner):
+    task = AsyncMock()
+    action_runner.register_tasks([task])
+    action_runner.run()
+    task.assert_awaited_once_with()
+
+
+def test_action_runner_runs_a_task_with_args(action_runner):
+    task = AsyncMock()
+    action_runner.register_tasks([(task, ("much", "args"))])
+    action_runner.run()
+    task.assert_awaited_once_with("much", "args")
+
+
+def test_action_runner_runs_a_task_with_kwargs(action_runner):
+    task = AsyncMock()
+    action_runner.register_tasks([(task, (), {"wow": "keyword", "many": "args"})])
+    action_runner.run()
+    task.assert_awaited_once_with(wow="keyword", many="args")
+
+
+def test_action_runner_runs_a_task_with_args_and_kwargs(action_runner):
+    task = AsyncMock()
+    action_runner.register_tasks([(task, ("much", "args"), {"wow": "keyword", "many": "args"})])
+    action_runner.run()
+    task.assert_awaited_once_with("much", "args", wow="keyword", many="args")
+
+
+def test_action_runner_runs_concurrent_tasks(action_runner):
+    tasks = [AsyncMock() for _ in range(10)]
+    action_runner.register_tasks(tasks)
+    action_runner.run()
+    for task in tasks:
+        task.assert_awaited_once_with()
+
+
+def test_action_runner_supports_counting(action_runner):
+    async def side_effect(self):
+        await self._incr_counter()
+
+    task = AsyncMock(side_effect=side_effect)
+    action_runner.register_tasks([(task, [action_runner])])
+    action_runner.run()
+    assert action_runner._counter == 1
+
+
+def test_action_runner_supports_concurrent_counts(action_runner):
+    async def side_effect(self):
+        await self._incr_counter()
+
+    tasks = [AsyncMock(side_effect=side_effect) for _ in range(10)]
+    action_runner.register_tasks(zip(tasks, [[action_runner]] * 10))
+    action_runner.run()
+    assert action_runner._counter == 10
+
+
+@pytest.mark.parametrize("signum", [signal.SIGINT, signal.SIGTERM])
+def test_action_runner_finishes_on_signals(signum, action_runner):
+    async def side_effect():
+        os.kill(os.getpid(), signum)
+        await asyncio.sleep(1e3)
+
+    task = AsyncMock(side_effect=side_effect)
+    action_runner.register_tasks([task])
+    action_runner.run()
+
+
+def test_action_runner_finishes_on_tasks_cancellation(action_runner, mocker):
+    async def side_effect():
+        for task in asyncio.all_tasks():
+            task.cancel()
+        await asyncio.sleep(1e3)
+
+    logger_mock = mocker.patch("walt.action_runners.logger")
+    task = AsyncMock(side_effect=side_effect)
+    action_runner.register_tasks([task])
+    action_runner.run()
+    logger_mock.info.assert_called_once()
---
+++ walt/action_runners.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of walt
+# https://github.com/scorphus/walt
+
+# Licensed under the BSD-3-Clause license:
+# https://opensource.org/licenses/BSD-3-Clause
+# Copyright (c) 2021, Pablo S. Blum de Aguiar <scorphus@gmail.com>
+
+from walt import logger
+
+import asyncio
+import signal
+
+
+class ActionRunnerBase:
+    """ActionRunnerBase is a base class for action runners"""
+
+    def __init__(self):
+        self._loop = asyncio.get_event_loop()
+        self._tasks = []
+        for signum in (signal.SIGINT, signal.SIGTERM):
+            self._loop.add_signal_handler(signum, self._sigint_handler)
+        self._counter, self._counter_lock = 0, asyncio.Lock()
+
+    def _sigint_handler(self):
+        logger.info("Stopping %s", self.__class__.__name__)
+        self._shutdown()
+
+    def _shutdown(self):
+        logger.debug("Stopping %s tasks", len(self._tasks))
+        for t in self._tasks:
+            t.cancel()
+
+    def run(self):
+        """run runs the action until completion"""
+        try:
+            self._loop.run_until_complete(self._run_action())
+        except asyncio.CancelledError:
+            logger.info("%s finished", self.__class__.__name__)
+
+    def _create_task(self, task, args=None, kwargs=None):
+        """_create_task creates the async `task` with arguments `args` and
+        keyword arguments `kwargs`"""
+        args = args if args is not None else []
+        kwargs = kwargs if kwargs is not None else {}
+        self._tasks.append(asyncio.create_task(task(*args, **kwargs)))
+
+    async def _run_action(self):
+        await asyncio.gather(*self._tasks, return_exceptions=True)
+
+    async def _incr_counter(self):
+        logger.debug("Incrementing counter")
+        async with self._counter_lock:
+            self._counter += 1
